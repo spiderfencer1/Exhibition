9b89785df845585ac1ee47c36ef322653c11d22c
spiderfencer1 <spiderfencer@Abhinavs-MacBook-Pro.local>
2017-04-17 11:39:35 -0700

    next commit

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const messages = [
 	"We flew into Havana, Cuba.",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	""
+	"We got introduced to the town of Matanzas, Cuba.",
+	"We were introduced to a facility for disabled children in Cuba, and started painting it.",
+	"We painted more of the facility for disabled children.",
+	"We finished painting the facility, and talked about Cuba as a country, as well as freedom of speech around the world.",
+	"We were surprised with a trip to the Bay of Pigs to snorkel.",
+	"We taught English to some Cuban kids, and played sports with them.",
+	"We brought bags of food to Cuban families. This was the day that Abhi lost his temper. Sorry!",
+	"We spent the day at Veradero Beach.",
+	"We drove to Havana, and toured the city.",
+	"We took a car tour around Havana city.",
+	"We left Cuba. Goodbye!"
 ];
 const message = document.getElementById("message");
 
@@ -33,8 +33,13 @@ function display(text){
 	});
 }
 
+function render(src){
+ document.getElementById("img").src = src;
+}
+
 [1,2,3,4,5,6,7,8,9,10,11,12].forEach(function(num){
 	document.getElementById("day"+num).addEventListener("click",function(){
 		display(messages[num-1]);
+  render("images/" + num + ".jpg");
 	});
 });
